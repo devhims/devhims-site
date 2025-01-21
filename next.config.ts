de664df48b7319c44a5dd12d1aad7b5466938988
Himanshu Gupta <thinktank.himanshu@gmail.com>
2025-01-21 23:01:42 +0530

    ppr test for performance

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: true,
+    inlineCss: true,
+    reactCompiler: true,
+  },
   images: {
     minimumCacheTTL: 31536000,
     remotePatterns: [
