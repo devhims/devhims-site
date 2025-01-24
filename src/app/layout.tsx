d7edfd5937e28fcc2242feebb9f26834415d7653
Himanshu Gupta <thinktank.himanshu@gmail.com>
2025-01-24 22:50:53 +0530

    Added vercel speed insights package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import './globals.css';
 
@@ -41,6 +42,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
         <Toaster richColors />
       </body>
     </html>
